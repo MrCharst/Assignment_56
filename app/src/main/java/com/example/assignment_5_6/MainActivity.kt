a212b943b7e133f559005f7e5ddb964fdad60d5a
Lê Minh Hiếu <minhhieu719@gmail.com>
2022-03-17 13:59:07 +0700

    submit

--- app/src/main/java/com/example/assignment_5_6/MainActivity.kt
+++ app/src/main/java/com/example/assignment_5_6/MainActivity.kt
@@ -6,14 +6,12 @@ import com.example.assignment_5_6.ui.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.test)
+        setContentView(R.layout.activity_main)
+
         supportFragmentManager.beginTransaction().replace(R.id.fragmentMain, MainFragment())
             .commitAllowingStateLoss()
 
     }
-
-
 }
---
+++ app/src/main/java/com/example/assignment_5_6/model/ItemRecycler.kt
@@ -0,0 +1,3 @@
+package com.example.assignment_5_6.model
+
+data class ItemRecycler(var name: String)
